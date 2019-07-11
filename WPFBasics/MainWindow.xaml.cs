bac0850ba4b569679491a768a2064a899e9df57b
= <=>
2019-07-11 19:20:04 +0500

    Added some controllers

--- WPFBasics/MainWindow.xaml.cs
+++ WPFBasics/MainWindow.xaml.cs
@@ -24,5 +24,40 @@ namespace WPFBasics
         {
             InitializeComponent();
         }
+
+        private void ApplyBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"The description is: { this.descripText.Text}");
+        }
+
+        private void ResetBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.weldcb.IsChecked = this.assemblycb.IsChecked = this.plasmacb.IsChecked = this.lasercb.IsChecked = this.purchasecb.IsChecked = this.lathecb.IsChecked =
+                this.drilcb.IsChecked = this.foldcb.IsChecked = this.rollcb.IsChecked = this.sawcb.IsChecked = false;
+        }
+
+        public void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            this.lengthText.Text += ((CheckBox)sender).Content;
+        }
+
+        private void ComboBox_Selected(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.noteText == null)
+                return;
+            var combo = (ComboBox)sender;
+            var value = (ComboBoxItem)combo.SelectedValue;
+            this.noteText.Text = (string)value.Content;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ComboBox_Selected(this.ComboBoxContent, null);
+        }
+
+        private void SupplierName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.massText.Text = this.supplierName.Text;
+        }
     }
 }
